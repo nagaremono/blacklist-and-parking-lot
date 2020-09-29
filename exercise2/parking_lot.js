994e64b0e2e0eb551ec5799c347dbff11b6b593a
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-09-29 17:24:54 +0800

    add interactive command input

--- exercise2/parking_lot.js
+++ exercise2/parking_lot.js
@@ -8,34 +8,34 @@ const createSearchResultMessage = (resultArray) => {
   return resultArray ? resultArray.join(', ') : 'Not Found';
 };
 
-const runCommand = (command, ...arguments) => {
+const runCommand = (command, arguments) => {
   let message, result;
 
   switch (command) {
     case 'create_parking_lot':
-      result = main.createParkingSlot(arguments[1]);
+      result = main.createParkingSlot(...arguments);
       message = `Created a parking lot with ${result} slots`;
       break;
     case 'park':
-      result = main.park(arguments[1], arguments[2]);
+      result = main.park(...arguments);
       message = result
         ? `Allocated slot number: ${result}`
         : 'Sorry, parking lot is full';
       break;
     case 'leave':
-      result = main.unpark(arguments[1]);
+      result = main.unpark(...arguments);
       message = `Slot number ${result} is free`;
       break;
     case 'registration_numbers_for_cars_with_colour':
-      result = main.findRegistrationNumbersByColor(arguments[1]);
+      result = main.findRegistrationNumbersByColor(...arguments);
       message = createSearchResultMessage(result);
       break;
     case 'slot_numbers_for_cars_with_colour':
-      result = main.findSlotNumbersByColor(arguments[1]);
+      result = main.findSlotNumbersByColor(...arguments);
       message = createSearchResultMessage(result);
       break;
     case 'slot_number_for_registration_number':
-      result = main.findSlotNumbersByRegistrationNumber(arguments[1]);
+      result = main.findSlotNumbersByRegistrationNumber(...arguments);
       message = createSearchResultMessage(result);
       break;
     default:
@@ -63,10 +63,29 @@ if (process.argv.length === 3) {
 
     for await (const line of rl) {
       const splittedCommand = line.split(' ');
-      const resultMessage = runCommand(splittedCommand[0], ...splittedCommand);
+      const resultMessage = runCommand(
+        splittedCommand[0],
+        splittedCommand.slice(1)
+      );
       console.log(resultMessage);
     }
   };
 
   read();
+} else {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.log('Please input commands');
+
+  rl.on('line', (commandInput) => {
+    const splittedCommand = commandInput.split(' ');
+    const resultMessage = runCommand(
+      splittedCommand[0],
+      splittedCommand.slice(1)
+    );
+    console.log(resultMessage);
+  });
 }
