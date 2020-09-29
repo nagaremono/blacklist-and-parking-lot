a5557e378904142f16f9801c15f201bcf1b88156
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-09-29 13:57:42 +0800

    shorten slightly redundant lines

--- exercise2/parking_lot.js
+++ exercise2/parking_lot.js
@@ -15,13 +15,14 @@ const main = (() => {
 
     const emptySlot = parkingSlots.findIndex((slot) => slot === null);
 
-    if (emptySlot === -1) return null;
+    if (emptySlot === -1) {
+      return null;
+    }
 
     car.slotNumber = emptySlot + 1;
 
     const updatedParkingSlots = [...parkingSlots];
     updatedParkingSlots[emptySlot] = car;
-
     parkingSlots = updatedParkingSlots;
 
     return emptySlot + 1;
@@ -32,27 +33,25 @@ const main = (() => {
     updatedParkingSlots[parkSlot - 1] = null;
     parkingSlots = updatedParkingSlots;
 
-    return parkSlot - 1;
+    return parkSlot;
   };
 
   const status = () => parkingSlots;
 
   const findRegistrationNumbersByColor = (color) => {
-    const carsWithCertainColor = parkingSlots.filter((car) => {
-      if (car) return car.color === color;
-    });
-
-    const registrationNumbers = carsWithCertainColor.map(
-      (car) => car.registrationNumber
-    );
+    const registrationNumbers = parkingSlots
+      .filter((car) => {
+        if (car) return car.color === color;
+      })
+      .map((car) => car.registrationNumber);
 
     return registrationNumbers.length === 0 ? null : registrationNumbers;
   };
 
   const findSlotNumbers = (filterFunction) => {
-    const carsWithCertainColor = parkingSlots.filter(filterFunction);
-
-    const slotNumbers = carsWithCertainColor.map((car) => car.slotNumber);
+    const slotNumbers = parkingSlots
+      .filter(filterFunction)
+      .map((car) => car.slotNumber);
 
     return slotNumbers.length === 0 ? null : slotNumbers;
   };
@@ -83,15 +82,3 @@ const main = (() => {
     findSlotNumbersByRegistrationNumber,
   };
 })();
-
-main.createParkingSlot(10);
-
-main.park('23454253', 'red');
-main.park('23454253', 'blue');
-main.park('23454253', 'red');
-main.park('234ss54253', 'yellow');
-main.park('23454253', 'red');
-
-console.log(main.findSlotNumbersByColor('yellow'));
-console.log(main.findSlotNumbersByRegistrationNumber('23454253'));
-console.log(main.findRegistrationNumbersByColor('yellow'));
